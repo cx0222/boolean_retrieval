0ee91037c946b973b4b4554c98440a014a3df06a
Xuan Chen <77929197+cx0222@users.noreply.github.com>
2022-09-17 19:20:59 +0800

    Update exception.h
    add TAB macro;

--- exception.h
+++ exception.h
@@ -11,16 +11,17 @@
 #define COLOR_GREEN "\033[0;32m"
 #define COLOR_YELLOW "\033[0;33m"
 #define COLOR_BLUE "\033[0;34m"
+#define TAB "\t"
 
 struct cannot_open_file_exception : std::exception {
     [[nodiscard]] const char *what() const noexcept final {
-        return COLOR_RED "cannot_open_file_exception\n";
+        return COLOR_RED TAB "cannot_open_file_exception\n";
     }
 };
 
 struct invalid_argument_exception : std::exception {
     [[nodiscard]] const char *what() const noexcept final {
-        return COLOR_RED "invalid_argument_exception\n";
+        return COLOR_RED TAB "invalid_argument_exception\n";
     }
 };
 
